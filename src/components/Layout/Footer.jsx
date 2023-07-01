99a16b23ec6da5639900a0d6cb2e29aec7174626
Vladislav <vladgm233223@gmail.com>
2023-07-01 16:35:07 +0300

    add footer section

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -1,3 +1,79 @@
+import Navigation from "./Navigation";
+
+import certified from "../../assets/High-Pressure-Certified-Logo.png";
+import familyLogo from "../../assets/family-owned-business-logo.png";
+
+import {
+  BsTiktok,
+  BsInstagram,
+  BsTwitter,
+  BsFacebook,
+  BsPinterest,
+} from "react-icons/bs";
+
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="footer-wrapper">
+          <div className="socials">
+            <a
+              className="svg-btn socials-btn"
+              href="https://www.pinterest.com/ilovegoodfoods/"
+            >
+              <BsPinterest />
+            </a>
+
+            <a
+              href="https://www.instagram.com/goodfoods/"
+              className="svg-btn socials-btn"
+            >
+              <BsInstagram />
+            </a>
+            <a
+              href="https://twitter.com/Goodfood_CA"
+              className="svg-btn socials-btn"
+            >
+              <BsTwitter />
+            </a>
+            <a
+              href="https://www.tiktok.com/@goodfoodsgroup"
+              className="svg-btn socials-btn"
+            >
+              <BsTiktok />
+            </a>
+            <a
+              href="https://www.facebook.com/GoodFoodsGroup"
+              className="svg-btn socials-btn"
+            >
+              <BsFacebook />
+            </a>
+          </div>
+          <Navigation />
+          
+          <div className="logo-container">
+            <a href="/">
+              <img
+                className="logo"
+                src={certified}
+                alt="logo"
+                height="80px"
+                width="80px"
+              />
+            </a>
+            <a href="/">
+              <img
+                className="logo family-logo"
+                src={familyLogo}
+                alt="logo"
+                height="80px"
+                width="110px"
+              />
+            </a>
+          </div>
+          <p className="copyright">© 2021 Good Foods Group</p>
+        </div>
+      </div>
+    </footer>
+  );
 }
--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -3,38 +3,28 @@ import { AiOutlineShoppingCart } from "react-icons/ai";
 import { BsSearchHeart } from "react-icons/bs";
 import { HiMenuAlt1 } from "react-icons/hi";
 
+import Navigation from "./Navigation";
+
 export default function Header() {
   return (
     <header className="header">
       <div className="container">
         <div className="header-wrapper">
+          <button className="burger-menu svg-btn">
+            <HiMenuAlt1 />
+          </button>
           <div className="logo-container">
             <a href="/">
               <img
                 className="logo"
                 src={logo}
                 alt="logo"
-                width="150px"
+                width="120px"
                 height="60px"
               />
             </a>
           </div>
-          <nav className="nav">
-            <ul className="nav-list">
-              <li className="nav-list-item">
-                <a href="/products">Products</a>
-              </li>
-              <li className="nav-list-item">
-                <a href="/good-eats">Good Eats</a>
-              </li>
-              <li className="nav-list-item">
-                <a href="/about">About</a>
-              </li>
-              <li className="nav-list-item">
-                <a href="/contact">Contact</a>
-              </li>
-            </ul>
-          </nav>
+          <Navigation />
           <div className="header-buttons">
             <button className="shopping-cart svg-btn">
               <AiOutlineShoppingCart />
@@ -44,9 +34,6 @@ export default function Header() {
             </button>
             <button className="shop-now orange-btn">Shop Now</button>
           </div>
-          <button className="burger-menu svg-btn">
-            <HiMenuAlt1 />
-          </button>
         </div>
       </div>
     </header>
